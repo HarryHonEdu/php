7834dbf84b45e0f97e9fbd01dd66fc0cc9960507
Harry Hon <harryhon0620@e.neivce.edu.my>
2024-11-13 13:16:00 +0800

    love

---
+++ Week1/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My First PHP!</title>
+    </head>
+    <body>
+        <?php
+            echo "Harry Hon";
+            
+        ?>
+
+    </body>
+</html>
